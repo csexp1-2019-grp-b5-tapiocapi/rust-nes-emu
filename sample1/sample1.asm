bd9540ea4c1aa7429a8a688165c3dde07d8ef0ea
GGBBB2000 <ggbbb@mineo.jp>
2019-07-21 17:28:48 +0900

    Rust format

--- sample1/sample1.asm
+++ sample1/sample1.asm
@@ -5,7 +5,7 @@
 .setcpu		"6502"
 .autoimport	on
 
-; iNESƒwƒbƒ_
+; iNESãƒ˜ãƒƒãƒ€
 .segment "HEADER"
 	.byte	$4E, $45, $53, $1A	; "NES" Header
 	.byte	$02			; PRG-BANKS
@@ -16,18 +16,18 @@
 	.byte	$00, $00, $00, $00	; 
 
 .segment "STARTUP"
-; ƒŠƒZƒbƒgŠ„‚è‚İ
+; ãƒªã‚»ãƒƒãƒˆå‰²ã‚Šè¾¼ã¿
 .proc	Reset
 	sei
 	ldx	#$ff
 	txs
 
-; ƒXƒNƒŠ[ƒ“ƒIƒt
+; ã‚¹ã‚¯ãƒªãƒ¼ãƒ³ã‚ªãƒ•
 	lda	#$00
 	sta	$2000
 	sta	$2001
 
-; ƒpƒŒƒbƒgƒe[ƒuƒ‹‚Ö“]‘—(BG—p‚Ì‚İ“]‘—)
+; ãƒ‘ãƒ¬ãƒƒãƒˆãƒ†ãƒ¼ãƒ–ãƒ«ã¸è»¢é€(BGç”¨ã®ã¿è»¢é€)
 	lda	#$3f
 	sta	$2006
 	lda	#$00
@@ -41,13 +41,13 @@ copypal:
 	dey
 	bne	copypal
 
-; ƒl[ƒ€ƒe[ƒuƒ‹‚Ö“]‘—(‰æ–Ê‚Ì’†‰›•t‹ß)
+; ãƒãƒ¼ãƒ ãƒ†ãƒ¼ãƒ–ãƒ«ã¸è»¢é€(ç”»é¢ã®ä¸­å¤®ä»˜è¿‘)
 	lda	#$21
 	sta	$2006
 	lda	#$c9
 	sta	$2006
 	ldx	#$00
-	ldy	#$0d		; 13•¶š•\¦
+	ldy	#$0d		; 13æ–‡å­—è¡¨ç¤º
 copymap:
 	lda	string, x
 	sta	$2007
@@ -55,30 +55,30 @@ copymap:
 	dey
 	bne	copymap
 
-; ƒXƒNƒ[ƒ‹İ’è
+; ã‚¹ã‚¯ãƒ­ãƒ¼ãƒ«è¨­å®š
 	lda	#$00
 	sta	$2005
 	sta	$2005
 
-; ƒXƒNƒŠ[ƒ“ƒIƒ“
+; ã‚¹ã‚¯ãƒªãƒ¼ãƒ³ã‚ªãƒ³
 	lda	#$08
 	sta	$2000
 	lda	#$1e
 	sta	$2001
 
-; –³ŒÀƒ‹[ƒv
+; ç„¡é™ãƒ«ãƒ¼ãƒ—
 mainloop:
 	jmp	mainloop
 .endproc
 
-; ƒpƒŒƒbƒgƒe[ƒuƒ‹
+; ãƒ‘ãƒ¬ãƒƒãƒˆãƒ†ãƒ¼ãƒ–ãƒ«
 palettes:
 	.byte	$0f, $00, $10, $20
 	.byte	$0f, $06, $16, $26
 	.byte	$0f, $08, $18, $28
 	.byte	$0f, $0a, $1a, $2a
 
-; •\¦•¶š—ñ
+; è¡¨ç¤ºæ–‡å­—åˆ—
 string:
 	.byte	"HELLO, WORLD!"
 
@@ -87,6 +87,6 @@ string:
 	.word	Reset
 	.word	$0000
 
-; ƒpƒ^[ƒ“ƒe[ƒuƒ‹
+; ãƒ‘ã‚¿ãƒ¼ãƒ³ãƒ†ãƒ¼ãƒ–ãƒ«
 .segment "CHARS"
 	.incbin	"character.chr"
--- src/ram.rs
+++ src/ram.rs
@@ -7,9 +7,14 @@ impl Ram {
         return Ram { ram: vec![0; size] };
     }
 
-    pub fn reset(&self) {}
-    pub fn write(&self) {}
+    pub fn reset(&mut self) {
+        self.ram = vec![0; self.ram.len()];
+    }
+
+    pub fn write(&mut self, addr: u16, data: u8) {
+        self.ram[addr as usize] = data;
+    }
     pub fn read(&self, addr: u16) -> u8 {
-        0
+        self.ram[addr as usize]
     }
 }
